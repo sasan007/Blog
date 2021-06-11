34306426730d98f01ad3f7c86d8cb14c6fb98d13
Home <Home@DESKTOP-OCNR93M>
2021-06-11 16:11:10 +0430

    BaseDbContext.cs Add

---
+++ Golrang.Framework/Infra/BaseDbContext.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Golrang.Framework.Infra
+{
+    public abstract class BaseDbContext : DbContext
+    {
+        public BaseDbContext(DbContextOptions options)
+        : base(options)
+        {
+        }
+        public override EntityEntry Add(object entity)
+        {
+            base.Add(entity);
+            base.SaveChanges();
+            return Entry(entity);
+        }
+        public override EntityEntry Update(object entity)
+        {
+            base.Update(entity);
+            base.SaveChanges();
+            return Entry(entity);
+        }
+        public override EntityEntry Remove(object entity)
+        {
+            base.Remove(entity);
+            base.SaveChanges();
+            return Entry(null);
+        }
+    }
+}
