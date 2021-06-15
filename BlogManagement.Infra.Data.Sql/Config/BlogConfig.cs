32e763c44a960ca0d04e958ad8b965e285312e18
Sasan <sasankarimi92@gmail.com>
2021-06-15 09:25:20 +0430

    BaseConfig

--- BlogManagement.Infra.Data.Sql/Config/BlogConfig.cs
+++ BlogManagement.Infra.Data.Sql/Config/BlogConfig.cs
@@ -1,19 +1,24 @@
 ﻿using BlogManagement.Core.Domain.Domain;
+using Golrang.Framework.Infra;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace BlogManagement.Infra.Data.Sql.Config
 {
-    public class BlogConfig : IEntityTypeConfiguration<Blog>
+    public class BlogConfig : BaseConfig<Blog>
     {
-        public void Configure(EntityTypeBuilder<Blog> builder)
-        {
-            builder.Property(c => c.Name).IsRequired().HasMaxLength(100);
-            builder.Property(c => c.EnName).IsRequired().HasMaxLength(100);
-            builder.Property(c => c.Desciption).IsRequired().HasMaxLength(500);
-            builder.Property(p => p.RowVersion).IsConcurrencyToken();
+        //public void Configure(EntityTypeBuilder<Blog> builder)
+        //{
+        //    builder.Property(c => c.Name).IsRequired().HasMaxLength(100);
+        //    builder.Property(c => c.EnName).IsRequired().HasMaxLength(100);
+        //    builder.Property(c => c.Desciption).IsRequired().HasMaxLength(500);
+        //    builder.Property(p => p.RowVersion).IsConcurrencyToken();
 
-            builder.ToTable("Blog");
+        //    builder.ToTable("Blog");
+        //}
+        public override void Handle(EntityTypeBuilder<Blog> builder)
+        {
+
         }
     }
 }
--- Golrang.Framework/Infra/BaseConfig.cs
+++ Golrang.Framework/Infra/BaseConfig.cs
@@ -9,11 +9,14 @@ using System.Threading.Tasks;
 
 namespace Golrang.Framework.Infra
 {
-    public class BaseConfig : IEntityTypeConfiguration<BaseEntity>
+    public abstract class BaseConfig<T> : IEntityTypeConfiguration<T> where T : BaseEntity
     {
-        public void Configure(EntityTypeBuilder<BaseEntity> builder)
+        public void Configure(EntityTypeBuilder<T> builder)
         {
             builder.Property(p => p.RowVersion).IsConcurrencyToken();
+            Handle(builder);
         }
+
+        public abstract void Handle(EntityTypeBuilder<T> builder);
     }
 }
